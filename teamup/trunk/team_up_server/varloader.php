ce7f1499913cda8bcc276c02103e1e5b80924fa1
Jukka Purma <jukka.purma@aalto.fi>
2012-09-03 08:08:13 +0000

    Added second layer of directories to recorder recordings.

--- teamup/trunk/team_up_server/varloader.php
+++ teamup/trunk/team_up_server/varloader.php
@@ -13,18 +13,23 @@ $base='uploads';
 $audio = $_FILES['voice']['tmp_name'];
 $class_id = $_POST['class_id'];
 $record_id = $_POST['record_id'];
+$class_hash= md5($class_id);
+$dir1=substr($class_hash, 0,2);
 
 
-if (!file_exists($base.'/'.$class_id)) {
-    mkdir($base.'/'.$class_id);
+if (!file_exists($base.'/'.$dir1)) {
+    mkdir($base.'/'.$dir1);
+}
+if (!file_exists($base.'/'.$dir1.'/'.$class_id)) {
+    mkdir($base.'/'.$dir1.'/'.$class_id);
 }
 if (isset($_FILES['photo'])) {
     $picture = $_FILES['photo']['tmp_name'];
-    $pic_name=$base.'/'.$class_id.'/'.$record_id.'_pic.jpg';
+    $pic_name=$base.'/'.$dir1.'/'.$class_id.'/'.$record_id.'_pic.jpg';
     move_uploaded_file($picture, $pic_name);
     fwrite($log, "Wrote file ".$pic_name."\n");
 }
-$aud_name=$base.'/'.$class_id.'/'.$record_id.'_rec.mp3';
+$aud_name=$base.'/'.$dir1.'/'.$class_id.'/'.$record_id.'_rec.mp3';
 move_uploaded_file($audio, $aud_name);
 fwrite($log, "Wrote file ".$aud_name."\n");
 fclose($log);
